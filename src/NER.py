f79b243c60ac4784a939da910ae3a66e5a9af273
startupscaling <121744047+startupscaling@users.noreply.github.com>
2020-07-11 18:12:35 +0000

    Bump version to 0.1.3

--- src/NER.py
+++ src/NER.py
@@ -17,3 +17,39 @@ class NER:
     A class used to give solution to the second task of the homework.
     
     ...
+    
+    Attributes
+    ----------
+    rawdata : str
+        string that contains the name of the file where the data is.
+    
+    Methods
+    -------
+    read_ner_data():
+        Reads the data from the file.
+    
+    preprocess_data():
+        Puts the data in the Spacy training dataset way, which is:
+        "{content: str, annotations: {(start, end, tag)}}"
+    
+    model_trainer():
+        Trains the model using a blank spacy model and the training dataset,
+        plots the training loss and saves the model to the same path.
+        
+    model_evaluation():
+        Evaluates the model using the F1-Score, Recall and Precision for the
+        entities (how well did the model tagged the content).
+    
+    """
+    
+    def __init__(self, rawdata=None):
+        
+        """
+        Constructs all the necessary attributes for the ner object.
+
+        Parameters
+        ----------
+        rawdata : str
+            string that contains the name of the file where the data is.
+        """
+        
