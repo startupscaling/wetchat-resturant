35cb19c979664e2c1e91a1776413648ccfe7950d
startupscaling <121744047+startupscaling@users.noreply.github.com>
2020-07-12 14:59:44 +0000

    Bump version to 2.1

--- src/Sentiment.py
+++ src/Sentiment.py
@@ -14,3 +14,52 @@ class Sentiment:
         string that contains the name of the file where the data is.
     
     Methods
+    -------
+    read_sentiment_data():
+        Reads the data from the file.
+    
+    predict_sentiment():
+        Executes the model to predict the sentiment with the previosuly processed
+        data.
+    """
+    
+    def __init__(self, rawdata=None):
+        
+        """
+        Constructs all the necessary attributes for the sentiment object.
+
+        Parameters
+        ----------
+        rawdata : str
+            string that contains the name of the file where the data is.
+        """
+        
+        self.rawdata = rawdata
+        self.data = None
+        
+    def read_sentiment_data(self):
+        
+        """
+        Reads the data that contains the file which path is contained in rawdata.
+        
+        Returns
+        -------
+        A message indicating the data has been collected.
+        """
+        
+        with open(self.rawdata, 'r') as f:
+            self.data = f.readlines()
+            
+        return 'Data collected'
+        
+    def predict_sentiment(self):
+        
+        """
+        Executes the selected model to predict the sentiment on the text data.
+        
+        Returns
+        -------
+        None
+        """
+        
+        tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
