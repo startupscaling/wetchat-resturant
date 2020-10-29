7b1ed8c0c6609c0c2ad5d6f30794a78eb52f5f86
startupscaling <121744047+startupscaling@users.noreply.github.com>
2020-10-29 18:58:23 +0000

    Cosmetic: Adjust indent.

--- src/Translate.py
+++ src/Translate.py
@@ -92,4 +92,6 @@ class Translate:
         """
         Reads and preprocesses the data contained in the two files and assigns the 
         first NUM_LINES_TO_PROCESS of the file to a list.
-  
+        
+        Returns
+        -----
