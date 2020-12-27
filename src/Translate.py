1a2a9b74c23abd9bc7db33591f2045daf804f063
startupscaling <121744047+startupscaling@users.noreply.github.com>
2020-12-27 18:23:41 +0000

    box

--- src/Translate.py
+++ src/Translate.py
@@ -154,4 +154,4 @@ class Translate:
             google_result = google_translate.translate(input_to_translate, 'es')
 
 
-            Ableu = sentence_bleu(self.lang1[i].split(), aws_result['TranslatedText'].split()) # Here I'm referencing the other languag
+            Ableu = sentence_bleu(self.lang1[i].split(), aws_result['TranslatedText'].split()) # Here I'm referencing the other language data, so I have to use the index to extrac
