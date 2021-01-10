74e07fb84039ecb790b3733419e91cbd75797d28
startupscaling <121744047+startupscaling@users.noreply.github.com>
2021-01-10 23:04:54 +0000

    60

--- tests/test.py
+++ tests/test.py
@@ -2,4 +2,5 @@ import os
 import sys
 
 
-ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), 
+ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')
+sys.path.append(os.path.join(ROOT, 'src
