2dbf06212a4559501838bf26081b58e16bb03b2c
startupscaling <121744047+startupscaling@users.noreply.github.com>
2021-01-15 06:01:02 +0000

    Cleanup style

--- tests/test.py
+++ tests/test.py
@@ -3,4 +3,4 @@ import sys
 
 
 ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')
-sys.path.append(os.path.join(ROOT, 'src
+sys.path.append(os.path.join(ROOT, 'src')) #Need to run this code first in order to 
