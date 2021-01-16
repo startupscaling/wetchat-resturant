a70bcf6ce6773f8ce57bff3da948bd3d14b5f079
startupscaling <121744047+startupscaling@users.noreply.github.com>
2021-01-16 10:26:03 +0000

    - fixed typos

--- tests/test.py
+++ tests/test.py
@@ -3,4 +3,6 @@ import sys
 
 
 ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')
-sys.path.append(os.path.join(ROOT, 'src')) #Need to run this code first in order to 
+sys.path.append(os.path.join(ROOT, 'src')) #Need to run this code first in order to have the test and src file on the same path.
+
+fro
