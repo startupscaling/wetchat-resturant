57ad27062a4d99440feb4a896f75922f99ac1124
startupscaling <121744047+startupscaling@users.noreply.github.com>
2021-01-23 21:44:34 +0000

    update note

--- tests/test.py
+++ tests/test.py
@@ -12,4 +12,8 @@ from Translate import Translate
 def test_sentiment():
     
     sentiment = Sentiment('tiny_movie_reviews_dataset.txt')
-    assert sentiment.read_sentiment_data() == 'Data collecte
+    assert sentiment.read_sentiment_data() == 'Data collected'
+
+def test_ner():
+
+    ner = NER('Corona2
